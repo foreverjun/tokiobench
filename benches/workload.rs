e6b162ee88052348f808d9fafd077724ea488156
Igor Erin <igor.erin.a@gmail.com>
2024-11-07 23:14:03 +0300

    workload: yield loop

--- benches/workload.rs
+++ benches/workload.rs
@@ -10,11 +10,13 @@ use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use tokiobench::params;
 use tokiobench::rt;
 use tokiobench::{split, split::SplitType};
+use tokiobench::work;
 
-type BenchFn = fn(&[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>);
+type BenchFn = fn(&[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>, work: CallBack);
+type CallBack = fn () -> ();
 
 #[inline]
-fn work(nspawns: &[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>) {
+fn work(nspawns: &[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>, work: fn () -> ()) {
     for nspawn in nspawns.iter().cloned() {
         let rem = rem.clone();
         let tx = tx.clone();
@@ -25,6 +27,11 @@ fn work(nspawns: &[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>) {
                 let tx = tx.clone();
 
                 tokio::spawn(async move {
+                    for _ in 0..params::YIEDL_BOUND {
+                        std::hint::black_box(work());
+                        tokio::task::yield_now().await;
+                    }
+
                     if 1 == rem.fetch_sub(1, Relaxed) {
                         tx.send(()).unwrap();
                     }
@@ -34,7 +41,7 @@ fn work(nspawns: &[usize], tx: SyncSender<()>, rem: Arc<AtomicUsize>) {
     }
 }
 
-fn workload(bench_fn: BenchFn, st: SplitType, reverse: bool, name: &str, c: &mut Criterion) {
+fn workload(bench_fn: BenchFn, st: SplitType, name: &str, work: CallBack, c: &mut Criterion) {
     let (tx, rx) = mpsc::sync_channel(1000);
     let rem: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
 
@@ -42,16 +49,12 @@ fn workload(bench_fn: BenchFn, st: SplitType, reverse: bool, name: &str, c: &mut
 
     for (nspawn, nworkers, nsplit) in iproduct!(params::NSPAWN, params::NWORKERS, params::NSPLIT) {
         group.throughput(Throughput::Elements(nspawn as u64));
-        let mut workload = split::split(st, nspawn, nsplit);
-
-        if reverse {
-            workload.reverse();
-        };
+        let workload = split::split(st, nspawn, nsplit);
 
         let rt = rt::new(nworkers);
 
         group.bench_with_input(
-            format!("nspawn({nspawn})/nwork({nworkers})/nsplit({nsplit}, {st})/rev({reverse})"),
+            format!("nspawn({nspawn})/nwork({nworkers})/nsplit({nsplit}, {st})"),
             &(nspawn, nworkers),
             |b, &(nspawn, _)| {
                 b.iter(|| {
@@ -60,7 +63,7 @@ fn workload(bench_fn: BenchFn, st: SplitType, reverse: bool, name: &str, c: &mut
 
                     rem.store(nspawn, Relaxed);
                     rt.block_on(async {
-                        bench_fn(workload.as_ref(), tx, rem);
+                        bench_fn(workload.as_ref(), tx, rem, work);
 
                         rx.recv().unwrap();
                     });
@@ -71,22 +74,27 @@ fn workload(bench_fn: BenchFn, st: SplitType, reverse: bool, name: &str, c: &mut
 }
 
 fn spawn_workload_eq(c: &mut Criterion) {
-    workload(work, SplitType::Eq, false, "workload", c);
+    workload(work, SplitType::Eq, "workload", work::nothing, c);
 }
 
 fn spawn_workload_gradient(c: &mut Criterion) {
-    workload(work, SplitType::Gradient, false, "workload", c);
+    workload(work, SplitType::Gradient, "workload", work::nothing, c);
 }
 
-fn spawn_workload_reverse(c: &mut Criterion) {
-    workload(work, SplitType::Gradient, true, "workload", c);
+fn spawn_workload_eq_recstall(c: &mut Criterion) {
+    workload(work, SplitType::Eq, "workload_recstall", work::nothing, c);
+}
+
+fn spawn_workload_gradient_recstall(c: &mut Criterion) {
+    workload(work, SplitType::Gradient, "workload_recstall", work::nothing, c);
 }
 
 criterion_group!(
     spawn_benches,
     spawn_workload_eq,
     spawn_workload_gradient,
-    spawn_workload_reverse
-);
+    spawn_workload_eq_recstall,
+    spawn_workload_gradient_recstall
+ );
 
 criterion_main!(spawn_benches);
--- graphs/main.py
+++ graphs/main.py
@@ -3,20 +3,45 @@
 import json
 import pathlib as lpath
 import matplotlib.pyplot as plt
+import argparse
+import itertools as it
 
 TARGET_PATH = lpath.Path().absolute() / "target"
 
 CRITERION_PATH = TARGET_PATH / "criterion"
 PLOTS_PATH = TARGET_PATH / "plots"
 
-NWORKERS = [1, 2, 4, 6, 8, 10, 12]
-NSPAWN = [100, 1000, 10000, 100000, 1000000, 10000000]
-NSPLIT = [1, 2, 4, 6, 8, 10]
+NWORKERS: list[int] | None = None
+NSPAWN: list[int] | None = None
+NSPLIT: list[int] | None = None
+
+def set_benches(proffile: str):
+    global NWORKERS
+    global NSPAWN
+    global NSPLIT
+
+    match proffile:
+        case "full":
+            NWORKERS = [1, 2, 4, 6, 8, 10, 12]
+            NSPAWN = [100, 1000, 10000, 100000, 1000000, 10000000]
+            NSPLIT = [1, 2, 4, 6, 8, 10]
+        case "default":
+            NWORKERS = [1, 4, 8]
+            NSPAWN = [100, 10000, 1000000]
+            NSPLIT = [1, 2, 4]
+        case "maxvalonly":
+            NWORKERS = [12]
+            NSPAWN = [10000000]
+            NSPLIT = [10]
+
+def check_state():
+    if NWORKERS is None or NSPAWN is None or NSPLIT is None:
+        raise RuntimeError("Constants not init")
 
 NROWS = 3
 NCOLS = 2
 
-def nwork_nspawn(*, bench: str, path: lpath.Path, show: bool = True) -> None:
+def nwork_nspawn(*, bench: str, path: lpath.Path) -> None:
     plt.xlabel("nspawn")
     plt.ylabel("mean")
 
@@ -44,11 +69,10 @@ def nwork_nspawn(*, bench: str, path: lpath.Path, show: bool = True) -> None:
     plt.legend(legend)
     plt.savefig(path)
 
-    if show:
-        plt.show()
+    plt.clf()
 
 
-def nwork_nspawn_split(*, bench: str, path: lpath.Path, show: bool = True, tsplit: str, rev: bool) -> None:
+def nwork_nspawn_split(*, bench: str, path: lpath.Path, tsplit: str) -> None:
     plt.xlabel("nspawn")
     plt.ylabel("mean")
 
@@ -60,8 +84,7 @@ def nwork_nspawn_split(*, bench: str, path: lpath.Path, show: bool = True, tspli
             data_x: list[int] = []
             data_y: list[int] = []
             for nspawn in NSPAWN:
-                rev = "true" if rev else "false"
-                name = f"nspawn({nspawn})_nwork({nwork})_nsplit({nsplit}, {tsplit})_rev(false)"
+                name = f"nspawn({nspawn})_nwork({nwork})_nsplit({nsplit}, {tsplit})"
                 est_path = CRITERION_PATH / bench / name / "new" / "estimates.json"
 
                 instance = json.load(est_path.open())
@@ -85,21 +108,34 @@ def nwork_nspawn_split(*, bench: str, path: lpath.Path, show: bool = True, tspli
     plt.legend(legend)
     plt.savefig(path)
 
-    if show:
-        plt.show()
+    plt.clf()
 
 
 NSPAWN_NWORK = ["remote_rec", "remote_rec_stall", "spawn_current", "remote_stall", "remote_stall_rec", "spawn_current", "spawn_local" ]
-NSPAWN_NWORK_NSPLIT = ["workload"]
+NSPAWN_NWORK_NSPLIT = it.product(["workload", "workload_recstall"], ["Gradient", "Eq"])
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='graphpy',
+        description='Draw cringe graphs for my diploma',
+        epilog="Duty dies last")
+
+    parser.add_argument("profile")
+
+    args = parser.parse_args()
+
+    set_benches(args.profile)
+    check_state() # should be last for state verification
+
     lpath.Path(PLOTS_PATH).mkdir(mode=0o777, parents=False, exist_ok=True)
 
     plt.figure(figsize=(10,10))
-    bench_name = "workload"
 
-    # for bench_name in NSPAWN_NWORK:
-    # nwork_nspawn(bench=bench_name, path=PLOTS_PATH / bench_name, show=False)
+    for bench_name in NSPAWN_NWORK:
+        nwork_nspawn(bench=bench_name, path=PLOTS_PATH / bench_name)
 
-    nwork_nspawn_split(bench=bench_name, path=PLOTS_PATH / bench_name, show=False, tsplit="Gradient", rev=False)
+    for (bench_name, tsplit) in NSPAWN_NWORK_NSPLIT:
+        path = PLOTS_PATH / f"{bench_name}_{tsplit}"
+
+        nwork_nspawn_split(bench=bench_name, path=path, tsplit=tsplit)
 
