d3ccb46f296abbc4d13d7b7a7d98e4ffbdcf21df
Igor Erin <igor.erin@syntacore.com>
2024-11-13 08:39:23 +0300

    wip: workload (many outliers)

--- graphs/main.py
+++ graphs/main.py
@@ -5,6 +5,7 @@ import pathlib as lpath
 import matplotlib.pyplot as plt
 import argparse
 import itertools as it
+import numpy as np
 
 TARGET_PATH = lpath.Path().absolute() / "target"
 
@@ -12,37 +13,35 @@ CRITERION_PATH = TARGET_PATH / "criterion"
 PLOTS_PATH = TARGET_PATH / "plots"
 
 NWORKERS: list[int] | None = None
-NSPAWN: list[int] | None = None
+NSPAWN = 100000
 NSPLIT: list[int] | None = None
+
+def todo():
+    raise RuntimeError("maxval not supportted now")
 
 def set_benches(proffile: str):
     global NWORKERS
-    global NSPAWN
     global NSPLIT
 
     match proffile:
         case "full":
+            todo()
             NWORKERS = [1, 2, 4, 6, 8, 10, 12]
-            NSPAWN = [100, 1000, 10000, 100000, 1000000, 10000000]
             NSPLIT = [1, 2, 4, 6, 8, 10]
         case "default":
-            NWORKERS = [1, 4, 8]
-            NSPAWN = [100, 10000, 1000000]
-            NSPLIT = [1, 2, 4]
+            NWORKERS = [1, 2, 4, 8, 10, 12]
+            NSPLIT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         case "maxvalonly":
+            todo()
             NWORKERS = [12]
-            NSPAWN = [10000000]
             NSPLIT = [10]
 
 def check_state():
     if NWORKERS is None or NSPAWN is None or NSPLIT is None:
         raise RuntimeError("Constants not init")
 
-NROWS = 3
-NCOLS = 2
-
-def nwork_nspawn(*, bench: str, path: lpath.Path) -> None:
-    plt.xlabel("nspawn")
+def nwork_nspawn(*, bench: str, path: lpath.Path, tsplit: str) -> None:
+    plt.xlabel("nsplit")
     plt.ylabel("mean")
 
     legend = []
@@ -51,18 +50,18 @@ def nwork_nspawn(*, bench: str, path: lpath.Path) -> None:
         data_x: list[int] = []
         data_y: list[int] = []
 
-        for nspawn in NSPAWN:
-            name = f"nspawn({nspawn})_nwork({nwork})"
+        for nsplit in NSPLIT:
+            name = f"nspawn({NSPAWN})_nwork({nwork})_nsplit({nsplit}, {tsplit})"
             est_path = CRITERION_PATH / bench / name / "new" / "estimates.json"
 
             instance = json.load(est_path.open())
 
-            data_x.append(nspawn)
+            data_x.append(nsplit)
 
             mean = instance["mean"]["point_estimate"]
             data_y.append(mean)
 
-        plt.plot(data_x, data_y)
+        plt.errorbar(data_x, data_y, label=str(nsplit))
 
         legend.append(f"{nwork} workers")
 
@@ -70,46 +69,6 @@ def nwork_nspawn(*, bench: str, path: lpath.Path) -> None:
     plt.savefig(path)
 
     plt.clf()
-
-
-def nwork_nspawn_split(*, bench: str, path: lpath.Path, tsplit: str) -> None:
-    plt.xlabel("nspawn")
-    plt.ylabel("mean")
-
-    legend = []
-    fig, axs = plt.subplots(ncols=NCOLS, nrows=NROWS, constrained_layout=True)
-
-    for split_ind, nsplit in enumerate(NSPLIT):
-        for nwork in NWORKERS:
-            data_x: list[int] = []
-            data_y: list[int] = []
-            for nspawn in NSPAWN:
-                name = f"nspawn({nspawn})_nwork({nwork})_nsplit({nsplit}, {tsplit})"
-                est_path = CRITERION_PATH / bench / name / "new" / "estimates.json"
-
-                instance = json.load(est_path.open())
-
-                data_x.append(nspawn)
-
-                mean = instance["mean"]["point_estimate"]
-                data_y.append(mean)
-
-            row = split_ind // NCOLS
-            col = split_ind % NCOLS
-
-            print(row)
-            print(col)
-
-            axs[row, col].plot(data_x, data_y)
-            axs[row, col].set_title(f"split {nsplit}")
-
-            legend.append(f"{nwork} workers")
-
-    plt.legend(legend)
-    plt.savefig(path)
-
-    plt.clf()
-
 
 NSPAWN_NWORK = ["remote_rec", "remote_rec_stall", "spawn_current", "remote_stall", "remote_stall_rec", "spawn_current", "spawn_local" ]
 NSPAWN_NWORK_NSPLIT = it.product(["workload", "workload_recstall"], ["Gradient", "Eq"])
@@ -131,11 +90,10 @@ if __name__ == "__main__":
 
     plt.figure(figsize=(10,10))
 
-    for bench_name in NSPAWN_NWORK:
-        nwork_nspawn(bench=bench_name, path=PLOTS_PATH / bench_name)
+    # for bench_name in NSPAWN_NWORK:
+    #     nwork_nspawn(bench=bench_name, path=PLOTS_PATH / bench_name)
 
     for (bench_name, tsplit) in NSPAWN_NWORK_NSPLIT:
         path = PLOTS_PATH / f"{bench_name}_{tsplit}"
 
-        nwork_nspawn_split(bench=bench_name, path=path, tsplit=tsplit)
-
+        nwork_nspawn(bench=bench_name, path=path, tsplit=tsplit)
