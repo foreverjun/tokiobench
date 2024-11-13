989c36cb2646e16f625afe2ca3f9ac9643f001dd
Igor Erin <igor.erin.a@gmail.com>
2024-11-14 00:05:12 +0300

    refactor: more splits!!!

--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,6 @@
 pub const NS_WORKERS: [usize; 5] = [1, 2, 4, 8, 12];
 pub const NS_SPAWN: [usize; 10] = [100, 200, 250, 300, 500, 1000, 5000, 10_000, 50_000, 100_000];
-pub const NS_SPLIT: [usize; 7] = [1, 2, 4, 6, 8, 10, 12];
+pub const NS_SPLIT: [usize; 12] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 pub const N_SPAWN: usize = 100000;
 
