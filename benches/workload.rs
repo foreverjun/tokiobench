d73af3bc543d5ec6e7f640e70bd4e2bfe80bc388
Igor Erin <igor.erin.a@gmail.com>
2024-12-14 13:17:16 +0300

    wip(worload): await in task via mpsc

--- benches/workload.rs
+++ benches/workload.rs
@@ -1,47 +1,42 @@
 use cfg_if::cfg_if;
 use itertools::iproduct;
 
+use std::sync::mpsc;
+use std::time::Duration;
+
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 
 use tokiobench::params as p;
 use tokiobench::rt;
 
-
-fn nspawn() -> Vec<usize> {
-    const BOUND: usize = 10;
-    const MULTIPLYER: usize = 1000;
-
-    (1..BOUND + 1).map(|i| i * MULTIPLYER).collect()
-}
-
-fn nspawner() -> Vec<usize> {
-    const BOUND: usize = 20;
-
-    (1..BOUND + 1).collect()
-}
-
-fn workload(name: &str, c: &mut Criterion) {
+fn workload(name: &str, nspawn: &[usize], nspawner: &[usize], c: &mut Criterion) {
+    let (tx, rx) = mpsc::sync_channel(1);
     let mut group = c.benchmark_group(format!("workload/{name}"));
-    for (nspawn, nspawner) in iproduct!(nspawn(), nspawner()) {
-        let rt = rt::new(p::N_WORKERS);
 
-        let mut leaf_handles = (0..nspawner)
-            .map(|_| Vec::with_capacity(nspawn))
-            .collect::<Vec<_>>();
-        let mut root_handles = Vec::with_capacity(nspawner);
+    for (&nspawn, &nspawner) in iproduct!(nspawn, nspawner) {
+        let rt = rt::new(p::N_WORKERS);
 
         group.throughput(Throughput::Elements((nspawner * nspawn) as u64));
         group.bench_function(format!("nspawn({nspawn})/nspawner({nspawner})"), |b| {
-            b.iter(|| {
-                cfg_if!(if #[cfg(feature = "check")] {
-                    assert!(root_handles.is_empty());
-                    assert!(root_handles.capacity() == nspawner);
+            let leaf_handles = (0..nspawner)
+                .map(|_| Vec::with_capacity(nspawn))
+                .collect::<Vec<_>>();
+            let root_handles = Vec::with_capacity(nspawner);
 
-                    assert!(leaf_handles.iter().all(|i| i.is_empty()));
-                    assert!(leaf_handles.iter().all(|i| i.capacity() == nspawn));
-                });
+            b.iter_reuse(
+                (leaf_handles, root_handles),
+                |(mut leaf_handles, mut root_handles)| {
+                    cfg_if!(if #[cfg(feature = "check")] {
+                        assert!(root_handles.is_empty());
+                        assert!(root_handles.capacity() == nspawner);
 
-                rt.block_on(async {
+                        assert!(leaf_handles.iter().all(|i| i.is_empty()));
+                        assert!(leaf_handles.iter().all(|i| i.capacity() == nspawn));
+                    });
+
+                    let tx = tx.clone();
+                    let _gurad = rt.enter();
+
                     for mut leaf_handle in leaf_handles.drain(..) {
                         root_handles.push(tokio::spawn(async move {
                             for _ in 0..leaf_handle.capacity() {
@@ -54,26 +49,50 @@ fn workload(name: &str, c: &mut Criterion) {
                         }));
                     }
 
-                    for leaf_handle in root_handles.drain(..) {
-                        let mut leaf_handle = leaf_handle.await.unwrap();
+                    tokio::spawn(async move {
+                        for leaf_handle in root_handles.drain(..) {
+                            let mut leaf_handle = leaf_handle.await.unwrap();
 
-                        for handle in leaf_handle.drain(..) {
-                            handle.await.unwrap();
+                            for handle in leaf_handle.drain(..) {
+                                handle.await.unwrap();
+                            }
+
+                            leaf_handles.push(leaf_handle);
                         }
 
-                        leaf_handles.push(leaf_handle);
-                    }
-                });
-            });
+                        tx.send((leaf_handles, root_handles)).unwrap();
+                    });
+
+                    rx.recv().unwrap()
+                },
+            );
         });
     }
     group.finish();
 }
 
-fn bench_nothing(c: &mut Criterion) {
-    workload("nothing", c)
+fn bench_thousand(c: &mut Criterion) {
+    let nspawn: Vec<usize> = (1..10 + 1).map(|i| i * 1000).collect();
+    let nspawner: Vec<usize> = (1..20 + 1).collect();
+
+    workload("thousand", nspawn.as_ref(), nspawner.as_ref(), c)
 }
 
-criterion_group!(benches, bench_nothing);
+fn bench_hundred(c: &mut Criterion) {
+    let nspawn: Vec<usize> = (1..10 + 1).map(|i| i * 100).collect();
+    let nspawner: Vec<usize> = (1..20 + 1).collect();
+
+    workload("hundred", nspawn.as_ref(), nspawner.as_ref(), c)
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default()
+        .sample_size(200)
+        .measurement_time(Duration::from_secs(300))
+        .warm_up_time(Duration::from_secs(60));
+
+    targets = bench_hundred, bench_thousand
+);
 
 criterion_main!(benches);
