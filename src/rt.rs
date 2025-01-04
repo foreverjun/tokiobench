07a477f00a4af03520a0d57d07927223dda38001
IgorErin <igor.erin.a@gmail.com>
2025-01-04 12:54:06 +0300

    refactor: keep_alive for blocking threads

--- src/rt.rs
+++ src/rt.rs
@@ -1,18 +1,11 @@
 use tokio::runtime::{self, Runtime};
+use std::time::Duration;
 
 pub fn new(workers: usize, bloking: usize) -> Runtime {
     runtime::Builder::new_multi_thread()
         .max_blocking_threads(bloking)
         .worker_threads(workers)
-        .build()
-        .unwrap()
-}
-
-pub fn new_no_lifo(workers: usize, bloking: usize) -> Runtime {
-    runtime::Builder::new_multi_thread()
-        .max_blocking_threads(bloking)
-        .worker_threads(workers)
-        .disable_lifo_slot()
+        .thread_keep_alive(Duration::from_secs(10_000))
         .build()
         .unwrap()
 }
