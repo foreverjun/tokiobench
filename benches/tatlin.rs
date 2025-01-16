e6cf69259a547792dd7ee9e6ff3c929a32a3b800
IgorErin <igor.erin.a@gmail.com>
2025-01-16 19:11:49 +0300

    origin: add

--- benches/tatlin.rs
+++ benches/tatlin.rs
@@ -9,13 +9,7 @@ use tokiobench::bench::tatlin;
 
 use tokiobench::rt;
 
-fn bench(
-    name: &str,
-    nspawn: &[usize],
-    nspawner: &[usize],
-    nworker: &[usize],
-    c: &mut Criterion,
-) {
+fn bench(name: &str, nspawn: &[usize], nspawner: &[usize], nworker: &[usize], c: &mut Criterion) {
     let (tx, rx) = mpsc::sync_channel(1);
     let mut group = c.benchmark_group(format!("tatlin/{name}"));
 
@@ -45,10 +39,62 @@ fn bench(
     group.finish();
 }
 
+fn bench_origin(
+    name: &str,
+    nspawn: &[usize],
+    nspawner: &[usize],
+    nworker: &[usize],
+    c: &mut Criterion,
+) {
+    let (tx, rx) = mpsc::sync_channel(1);
+    let mut group = c.benchmark_group(format!("tatlin/{name}"));
+
+    for (&nspawn, &nspawner, &nworker) in iproduct!(nspawn, nspawner, nworker) {
+        let rt = rt::new(nworker, 1);
+
+        group.throughput(Throughput::Elements((nspawn * nspawner) as u64));
+        group.bench_function(
+            format!("nspawn({nspawn})/nspawner({nspawner})/nworker({nworker})"),
+            |b| {
+                let (root_handles, leaf_handles) = tatlin::mk_handles(nspawner, nspawn);
+
+                b.iter_reuse(
+                    (root_handles, leaf_handles),
+                    |(root_handles, leaf_handles)| {
+                        let tx = tx.clone();
+
+                        let _gurad = rt.enter();
+                        tatlin::origin::run_origin(
+                            nspawner,
+                            nspawn,
+                            tx,
+                            root_handles,
+                            leaf_handles,
+                        );
+
+                        rx.recv().unwrap()
+                    },
+                );
+            },
+        );
+    }
+    group.finish();
+}
+
 macro_rules! benches {
     ($expression:tt) => {
         pub fn local(c: &mut Criterion) {
             bench(
+                concat!($expression, "/local"),
+                &nspawn(),
+                &nspawner(),
+                &nworker(),
+                c,
+            )
+        }
+
+        pub fn local_origin(c: &mut Criterion) {
+            bench_origin(
                 concat!($expression, "/local"),
                 &nspawn(),
                 &nspawner(),
@@ -94,7 +140,7 @@ criterion_group!(
         .measurement_time(Duration::from_secs(100))
         .warm_up_time(Duration::from_secs(3));
 
-    targets = line::local
+    targets = line::local_origin
 );
 
 criterion_main!(benches);
--- src/bench/tatlin.rs
+++ src/bench/tatlin.rs
@@ -81,3 +81,54 @@ pub fn run(
         tx.send((root_handles, leaf_handles)).unwrap()
     });
 }
+
+pub mod origin {
+    use super::*;
+    use std::sync::Arc;
+
+    async fn task_type_1(n: usize, mut handles: LeafHandle) -> LeafHandle {
+        assert_eq!(n, handles.capacity());
+        assert!(handles.is_empty());
+
+        let data = Arc::new(vec![1u8; 1_000_000]);
+
+        for _ in 0..handles.capacity() {
+            handles.push(tokio::spawn(task_type_2(data.clone())));
+        }
+
+        for handle in handles.drain(..) {
+            handle.await.unwrap();
+        }
+
+        handles
+    }
+
+    async fn task_type_2(data: Arc<Vec<u8>>) {
+        drop(data);
+    }
+
+    type LeafHandle = Vec<JoinHandle<()>>;
+
+    pub fn run_origin(
+        _nspawner: usize,
+        _nspawn: usize,
+        tx: SyncSender<(RootHandles, LeafHandles)>,
+        mut root_handles: RootHandles,
+        mut leaf_handles: LeafHandles,
+    ) {
+        #[cfg(feature = "check")]
+        _precond_assert(_nspawn, _nspawner, &leaf_handles, &root_handles);
+
+        tokio::spawn(async move {
+            for leaf_handle in leaf_handles.drain(..) {
+                root_handles.push(tokio::spawn(task_type_1(_nspawn, leaf_handle)));
+            }
+
+            for leaf_handle in root_handles.drain(..) {
+                leaf_handles.push(leaf_handle.await.unwrap());
+            }
+
+            tx.send((root_handles, leaf_handles)).unwrap();
+        });
+    }
+}
